facbb8fe680a8f19a1de1883b2f273de473be640
delfigica <delfinamariacanas@gmail.com>
2023-05-09 02:40:05 -0300

    combar component and logic

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ScoreBar } from "./Components/ScoreBar/ScoreBar";
+import { SelectOne } from "./Components/SelectOne/SelectOne";
+import { Combat } from "./Components/Combat/Combat";
 
 import "./App.css";
-import { SelectOne } from "./Components/SelectOne/SelectOne";
-
 function App() {
   const [score, setScore] = useState(0);
-  const [selection, setSelection] = useState('')
+  const [selection, setSelection] = useState("");
+  const [computer, setComputer] = useState("");
+
+  useEffect(() => {
+    const chooseRandomly = (a, b) => {
+      return Math.random() < 0.5 ? a : b;
+    };
+    if (selection === "rock") {
+      setTimeout(() => {
+        setComputer(chooseRandomly("paper", "scissors"));
+      }, 2000);
+    } else if (selection === "paper") {
+      setTimeout(() => {
+        setComputer(chooseRandomly("rock", "scissors"));
+      }, 2000);
+    } else if (selection === "scissors") {
+      setTimeout(() => {
+        setComputer(chooseRandomly("paper", "rock"));
+      }, 2000);
+    }
+  }, [selection]);
 
   return (
     <div className="App">
       <ScoreBar score={score} />
-      <SelectOne setSelection={setSelection} />
+
+      {selection ? (
+        <Combat selection={selection} computer={computer} />
+      ) : (
+        <SelectOne setSelection={setSelection} />
+      )}
     </div>
   );
 }
--- src/Components/Combat/Combat.js
+++ src/Components/Combat/Combat.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React from "react";
 
-import './combat.css';
-export const Combat = () => {
+import "./combat.css";
+
+import paper from "../../assets/images/icon-paper.svg";
+import scissors from "../../assets/images/icon-scissors.svg";
+import rock from "../../assets/images/icon-rock.svg";
+
+export const Combat = ({ selection = "paper", computer = "rock" }) => {
+  const Paper = () => {
+    return (
+      <div className="combat-selection-box paper-box">
+        <img src={paper} />
+      </div>
+    );
+  };
+
+  const Scissors = () => {
+    return (
+      <div className="combat-selection-box scissors-box">
+        <img src={scissors} />
+      </div>
+    );
+  };
+
+  const Rock = () => {
+    return (
+      <div className="combat-selection-box rock-box">
+        <img src={rock} />
+      </div>
+    );
+  };
+
   return (
-    <div>Combat</div>
-  )
-}
+    <div className="combat-box">
+      <div className="cobat-item">
+        <p>You picked</p>
+        {selection === "paper" && <Paper />}
+        {selection === "rock" && <Rock />}
+        {selection === "scissors" && <Scissors />}
+      </div>
+      <div className="cobat-item">
+        <p>The house picked</p>
+        {computer === "paper" && <Paper />}
+        {computer === "rock" && <Rock />}
+        {computer === "scissors" && <Scissors />}
+        {computer === "" && <div className="combat-computer-loading"></div>}
+      </div>
+    </div>
+  );
+};
