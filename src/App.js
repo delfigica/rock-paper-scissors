ce89e0841cde74388474fc4d2799f6596eff7bcf
delfigica <delfinamariacanas@gmail.com>
2023-05-09 01:28:17 -0300

    select one component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { useState } from "react";
 import { ScoreBar } from "./Components/ScoreBar/ScoreBar";
 
 import "./App.css";
+import { SelectOne } from "./Components/SelectOne/SelectOne";
 
 function App() {
   const [score, setScore] = useState(0);
+  const [selection, setSelection] = useState('')
 
   return (
     <div className="App">
       <ScoreBar score={score} />
+      <SelectOne setSelection={setSelection} />
     </div>
   );
 }
--- src/Components/SelectOne/SelectOne.js
+++ src/Components/SelectOne/SelectOne.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
 
-import './select-one.css';
+import "./select-one.css";
 
-export const SelectOne = () => {
+import paper from "../../assets/images/icon-paper.svg";
+import scissors from "../../assets/images/icon-scissors.svg";
+import rock from "../../assets/images/icon-rock.svg";
+
+export const SelectOne = ({ setSelection }) => {
+
+  const handleSelection = (selection) => {
+    setSelection(selection);
+  };
+  
   return (
-    <div>SelectOne</div>
-  )
-}
+    <div className="select-box">
+      <div
+        className="paper-box option-box"
+        onClick={() => {
+          handleSelection("paper");
+        }}
+      >
+        <img src={paper} />
+      </div>
+      <div
+        className="scissors-box option-box"
+        onClick={() => {
+          handleSelection("scissors");
+        }}
+      >
+        <img src={scissors} />
+      </div>
+      <div
+        className="rock-box option-box"
+        onClick={() => {
+          handleSelection("rock");
+        }}
+      >
+        <img src={rock} />
+      </div>
+    </div>
+  );
+};
